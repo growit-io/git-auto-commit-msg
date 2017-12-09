3b7bed0fd1c2482adfc09e6bf18bcbba75e52242
Uwe Stuehler <ustuehler@growit.io>
2017-12-09 02:17:26 +0100

    wip: add/update index.test.js, package-lock.json and 1 more

---
+++ index.test.js
@@ -0,0 +1,19 @@
+const { execFileSync } = require('child_process');
+
+const PROGRAM_FILE = './index.js'
+
+test('exits successfully', () => {
+  expect(program().code).toBe(0);
+});
+
+function program(...argv) {
+  let result = {code: 0, error: null}
+  try {
+    result.stdout = execFileSync(PROGRAM_FILE, argv);
+  } catch (err) {
+    result.code = -1 // XXX
+    result.error = err
+  } finally {
+    return result
+  }
+}
