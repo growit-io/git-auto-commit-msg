88ad20dcd36bb8b0423693053a1ca72e0f6fdafd
Uwe Stuehler <ustuehler@users.noreply.github.com>
2017-12-09 04:02:22 +0100

    feat: recognise docs (#3)

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # git-auto-commit-msg [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org) [![Build Status](https://travis-ci.org/growit-io/git-auto-commit-msg.svg?branch=master)](https://travis-ci.org/growit-io/git-auto-commit-msg)
 
-`git-auto-commit-msg` generates a conventional commit message from any changed files found in the working tree.
+`git-auto-commit-msg` attempts to generate a [conventional](https://conventionalcommits.org/) commit message from any changed files found in the working tree.
 
-## Example
-    $ rm -f file-[12345]
+## Installation
+    npm install -g git-auto-commit-msg
+
+## Examples
+By default, a simple summary of changed files is generated from the index.
+
+    $ for x in $(seq 1 3); do touch file-$x; done
+    $ git add -A
+    $ git-auto-commit-msg
+    add file-1, file-2 and 1 more
+
+Paths can be restricted by one or more `pathspec` arguments, even if other files are also present in the index.
+
     $ for x in $(seq 1 5); do touch file-$x; done
-    $ git add file-[123]
+    $ git add -A
     $ git-auto-commit-msg file-[12345]
     add file-1, file-2 and 1 more
+
+The `docs:` prefix is added if the changes are entirely limited to documentation files.
+
+    $ git-auto-commit-msg README.md CHANGELOG.md docs examples example
+    docs: update README.md
+
+## License
+See the [LICENSE](LICENSE) file.
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-const program = require('commander');
-const readline = require('readline');
-const shell = require('shelljs');
-const shellescape = require('shell-escape');
+const program = require('commander')
+const shell = require('shelljs')
+const shellescape = require('shell-escape')
 
 // Show these many paths and then append " and x more"
 const DEFAULT_SHOW_PATHS = 2
@@ -12,54 +11,72 @@ program
   .description('Generates a conventional commit message based on staged changes')
   .parse(process.argv)
 
-shell.exec(
-  'git status -s -- ' + shellescape(program.args),
-  {silent: true},
-  function (code, stdout, stderr) {
-    if (code != 0) {
-      throw new Error(stderr)
-    }
+const PATHSPEC_DOCS = ['README.md', 'CHANGELOG.md', 'docs', 'examples', 'example']
 
-    var added = []
-    var updated = []
-    var deleted = []
-    var lines = stdout.split(/\r?\n/);
-
-    for (var n in lines) {
-      var line = lines[n]
-      var file = line.slice(3)
-      var x = line[0] // tree -> index
-      var y = line[1] // index -> work tree
-
-      if (x === 'A') {
-        added.push(file)
-      } else if (x === 'M') {
-        updated.push(file)
-      } else if (x === 'D') {
-        deleted.push(file)
+commitmsg(program.args, function (msg) {
+  if (msg !== '') {
+    commitmsg(PATHSPEC_DOCS, function (docs) {
+      if (docs === msg) {
+        console.log('docs: ' + msg)
+      } else {
+        console.log(msg)
       }
-    }
-
-    var changes = added.concat(updated).concat(deleted);
-    if (changes.length === 0) {
-      // No changes, no commit message
-      return
-    }
-
-    var actions = []
-    if (added.length > 0) {
-      actions.push('add')
-    }
-    if (updated.length > 0) {
-      actions.push('update')
-    }
-    if (deleted.length > 0) {
-      actions.push('remove')
-    }
-
-    console.log(actionlist(actions) + ' ' + filelist(changes))
+    })
   }
-);
+})
+
+function commitmsg(pathspecs, cb) {
+  shell.exec(
+    'git status -s -- ' + shellescape(pathspecs),
+    {silent: true},
+    function (code, stdout, stderr) {
+      if (code != 0) {
+        throw new Error(stderr)
+      }
+      cb(formatStatus(stdout))
+    }
+  )
+}
+
+function formatStatus(status) {
+  var added = []
+  var updated = []
+  var deleted = []
+  var lines = status.split(/\r?\n/)
+
+  for (var n in lines) {
+    var line = lines[n]
+    var file = line.slice(3)
+    var x = line[0] // tree -> index
+
+    if (x === 'A') {
+      added.push(file)
+    } else if (x === 'M') {
+      updated.push(file)
+    } else if (x === 'D') {
+      deleted.push(file)
+    }
+  }
+
+  var changes = added.concat(updated).concat(deleted)
+  if (changes.length === 0) {
+    // No changes, no commit message
+    return
+  }
+
+  var actions = []
+  if (added.length > 0) {
+    actions.push('add')
+  }
+  if (updated.length > 0) {
+    actions.push('update')
+  }
+  if (deleted.length > 0) {
+    actions.push('remove')
+  }
+
+  return actionlist(actions) + ' ' + filelist(changes)
+}
 
 function actionlist(actions) {
   return actions.sort().join('/')
